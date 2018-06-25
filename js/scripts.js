0e67df9d7d9b8101761cfdbad9fb36b31b8f693f
Michael Chun <kho1995@gmail.com>
2018-06-25 13:07:49 +1200

    inital commit

---
+++ js/scripts.js
@@ -0,0 +1,76 @@
+/*
+Theme Name: IAMX
+Author: Trendy Theme
+Author URL: trendytheme.net
+*/
+
+/*
+    = Preloader
+    = Progress Bar
+
+*/
+
+jQuery(function ($) {
+
+    'use strict';
+
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+
+    $(window).ready(function() {
+        $('#pre-status').fadeOut();
+        $('#tt-preloader').delay(350).fadeOut('slow');
+    });
+
+    // -------------------------------------------------------------
+    // Progress Bar
+    // -------------------------------------------------------------
+
+    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+    // -------------------------------------------------------------
+    // More skill
+    // -------------------------------------------------------------
+    $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $('.chart').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor:'#68c3a3',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 140,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+    // -------------------------------------------------------------
+    // WOW JS
+    // -------------------------------------------------------------
+
+    (function () {
+
+        new WOW({
+
+            mobile:  false
+
+        }).init();
+
+    }());
+});
